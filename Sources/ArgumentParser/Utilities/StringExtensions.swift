52f42690d27fa28a9f84eed9778c80b0d383d8a9
Klaas Pieter Annema <kp@annema.me>
2020-02-28 15:11:07 +0100

    Add String#editDistance(to:)
    Uses levenshtein distance to determine how much two strings differ.
    
    See: https://en.wikipedia.org/wiki/Levenshtein_distance

--- Sources/ArgumentParser/Utilities/StringExtensions.swift
+++ Sources/ArgumentParser/Utilities/StringExtensions.swift
@@ -144,4 +144,51 @@ extension String {
       self[range].lowercased()
     }.joined(separator: String(separator))
   }
+  
+  /// Returns the edit distance between this string and the provided target string.
+  ///
+  /// Uses the Levenshtein distance algorithm internally.
+  ///
+  /// See: https://en.wikipedia.org/wiki/Levenshtein_distance
+  ///
+  /// Examples:
+  ///
+  ///     "kitten".editDistance(to: "sitting")
+  ///     // 3
+  ///     "bar".editDistance(to: "baz")
+  ///     // 1
+
+  func editDistance(to target: String) -> Int {
+    let rows = self.count
+    let columns = target.count
+    
+    if rows <= 0 || columns <= 0 {
+      return max(rows, columns)
+    }
+    
+    var matrix = Array(repeating: Array(repeating: 0, count: columns + 1), count: rows + 1)
+    
+    for row in 1...rows {
+      matrix[row][0] = row
+    }
+    for column in 1...columns {
+      matrix[0][column] = column
+    }
+    
+    for row in 1...rows {
+      for column in 1...columns {
+        let source = self[self.index(self.startIndex, offsetBy: row - 1)]
+        let target = target[target.index(target.startIndex, offsetBy: column - 1)]
+        let cost = source == target ? 0 : 1
+        
+        matrix[row][column] = Swift.min(
+          matrix[row - 1][column] + 1,
+          matrix[row][column - 1] + 1,
+          matrix[row - 1][column - 1] + cost
+        )
+      }
+    }
+    
+    return matrix.last!.last!
+  }
 }
