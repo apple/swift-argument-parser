c0ecae3a045d16e9cbb6b30c48ac001a20cea100
Richard Stelling <rich@lionheartapplications.com>
2020-04-14 11:01:05 +0100

    Improved transform `@Option` tests
    Added a test for the defaut error message and renamed object to imply the tests are for `@Options` only

--- Tests/ArgumentParserEndToEndTests/TransformEndToEndTests.swift
+++ Tests/ArgumentParserEndToEndTests/TransformEndToEndTests.swift
@@ -16,30 +16,39 @@ import ArgumentParser
 final class TransformEndToEndTests: XCTestCase {
 }
 
-fileprivate struct Foo: ParsableArguments {
+fileprivate struct FooOption: ParsableArguments {
 
     static var usageString: String = """
-    Usage: foo --string <int_str>
+    Usage: foo_option --string <int_str>
     """
+    
+    enum FooError: Error {
+        case outOfBounds
+    }
     
     @Option(help: ArgumentHelp("Convert string to integer", valueName: "int_str"),
             transform: { try convert($0) })
     var string: Int
     
     private static func convert(_ str: String) throws -> Int {
-        guard let converted = Int(argument: str) else { throw ValidationError("Could not convert to Int.") }
+        guard let converted = Int(argument: str) else { throw ValidationError("Could not transform to an Int.") }
+        guard converted < 1000 else { throw FooError.outOfBounds }
         return converted
     }
 }
 
 extension TransformEndToEndTests {
     func testTransform() throws {
-        AssertParse(Foo.self, ["--string", "42"]) { foo in
+        AssertParse(FooOption.self, ["--string", "42"]) { foo in
             XCTAssertEqual(foo.string, 42)
         }
     }
     
-    func testValidation_Fail() throws {
-        AssertFullErrorMessage(Foo.self, ["--string", "Forty Two"], "Error: Could not convert to Int.\n" + Foo.usageString)
+    func testValidation_Fail_CustomErrorMessage() throws {
+        AssertFullErrorMessage(FooOption.self, ["--string", "Forty Two"], "Error: Could not transform to an Int.\n" + FooOption.usageString)
+    }
+
+    func testValidation_Fail_DefaultErrorMessage() throws {
+        AssertFullErrorMessage(FooOption.self, ["--string", "4827"], "Error: The value '4827' is invalid for '--string <int_str>'\n" + FooOption.usageString)
     }
 }
