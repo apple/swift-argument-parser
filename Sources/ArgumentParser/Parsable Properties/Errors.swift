0a5d9d09fff985f5e1d479936885588e34b12336
Richard Stelling <richard@empiricalmagic.com>
2020-04-21 15:31:45 +0100

    Changing the access level of `ValidationError.message` (#119)
    * `ValidationError.message` is now public readable
    
    * Added a documentation line

--- Sources/ArgumentParser/Parsable Properties/Errors.swift
+++ Sources/ArgumentParser/Parsable Properties/Errors.swift
@@ -24,7 +24,10 @@ import let WinSDK.ERROR_BAD_ARGUMENTS
 /// An error type that is presented to the user as an error with parsing their
 /// command-line input.
 public struct ValidationError: Error, CustomStringConvertible {
-  var message: String
+  /// The error message represented by this instance, this string is presented to
+  /// the user when a `ValidationError` is thrown from either; `run()`,
+  /// `validate()` or a transform closure.
+  public internal(set) var message: String
   
   /// Creates a new validation error with the given message.
   public init(_ message: String) {
