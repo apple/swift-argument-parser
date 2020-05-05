3166c458f99e51892aab0675ec2d1efb373864ed
Nate Cook <natecook@apple.com>
2020-05-04 23:32:45 -0500

    Update error message to mention initializers (#133)

--- Sources/ArgumentParser/Parsable Types/ParsableArguments.swift
+++ Sources/ArgumentParser/Parsable Types/ParsableArguments.swift
@@ -222,9 +222,13 @@ internal let directlyInitializedError = """
   --------------------------------------------------------------------
   Can't read a value from a parsable argument definition.
 
-  This error indicates that a `ParsableCommand` or `ParsableArguments`
-  type is being initialized directly, instead of by calling `parse`,
-  `parseAsRoot`, or `main`. See the documentation for correct usage.
+  This error indicates that a property declared with an `@Argument`,
+  `@Option`, `@Flag`, or `@OptionGroup` property wrapper was neither
+  initialized to a value nor decoded from command-line arguments.
+
+  To get a valid value, either call one of the static parsing methods
+  (`parse`, `parseAsRoot`, or `main`) or define an initializer that
+  initializes _every_ property of your parsable type.
   --------------------------------------------------------------------
 
   """
