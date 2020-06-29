0cc0626ca5e76d096c3b36520c0a0f232e8f0e48
Kenny Schlagel <schlagelk@aetna.com>
2020-06-29 14:07:52 -0600

    Improve performance of Sequence.uniquified()

--- Sources/ArgumentParser/Utilities/SequenceExtensions.swift
+++ Sources/ArgumentParser/Utilities/SequenceExtensions.swift
@@ -9,14 +9,9 @@
 //
 //===----------------------------------------------------------------------===//
 
-extension Sequence where Element: Equatable {
-  func uniquified() -> [Element] {
-    var sequence = Array<Element>()
-    for element in self {
-      if !sequence.contains(element) {
-        sequence.append(element)
-      }
-    }
-    return sequence
+extension Sequence where Iterator.Element: Hashable {
+  func uniquified() -> [Iterator.Element] {
+    var seen: [Iterator.Element: Bool] = [:]
+    return self.filter { seen.updateValue(true, forKey: $0) == nil }
   }
 }
