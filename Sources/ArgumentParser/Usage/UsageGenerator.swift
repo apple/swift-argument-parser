5bfb39ac07561d05f23eb4f9ed48237674a1416e
Karoy Lorentey <klorentey@apple.com>
2021-02-16 07:30:31 -0800

    Generate useful synopsis for commands with many options (#275)
    ArgumentParser is configured not to emit detailed synopsis when it would contain more than a dozen entries. This makes sense; however, eliding all information makes the synopsis rather useless.
    
    While commands may have dozens of options, in most cases, only a few of them are required — so we can keep the synopsis short but still useful by only displaying the required parts.
    
    * Include all positional arguments in shortened synopsis

--- Sources/ArgumentParser/Usage/UsageGenerator.swift
+++ Sources/ArgumentParser/Usage/UsageGenerator.swift
@@ -41,6 +41,17 @@ extension UsageGenerator {
     case 0:
       return toolName
     case let x where x > 12:
+      // When we have too many options, keep required and positional arguments,
+      // but discard the rest.
+      let synopsis: [String] = definition.compactMap { argument in
+        guard argument.isPositional || !argument.help.options.contains(.isOptional) else {
+          return nil
+        }
+        return argument.synopsis
+      }
+      if !synopsis.isEmpty, synopsis.count <= 12 {
+        return "\(toolName) [<options>] \(synopsis.joined(separator: " "))"
+      }
       return "\(toolName) <options>"
     default:
       return "\(toolName) \(definition.synopsis.joined(separator: " "))"
