8a0863847d77f8a9de62eb47f53b5c1625b77abf
Miguel Perez <mperezojito@apple.com>
2021-04-22 11:59:52 -0400

    Expose ArgumentParserTestHelpers

--- Package.swift
+++ Package.swift
@@ -18,6 +18,9 @@ var package = Package(
         .library(
             name: "ArgumentParser",
             targets: ["ArgumentParser"]),
+        .library(
+            name: "ArgumentParserTestHelpers",
+            targets: ["ArgumentParserTestHelpers"]),
     ],
     dependencies: [],
     targets: [
