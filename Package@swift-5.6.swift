db52b8403e57eeb16711064b9bc2cb2dece6d8e2
Nate Cook <natecook@apple.com>
2022-08-26 20:10:49 -0500

    Add the Swift-DocC plugin to the 5.6 Package file

--- Package@swift-5.6.swift
+++ Package@swift-5.6.swift
@@ -22,7 +22,9 @@ var package = Package(
             name: "GenerateManualPlugin",
             targets: ["GenerateManualPlugin"]),
     ],
-    dependencies: [],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
+    ],
     targets: [
         // Core Library
         .target(
