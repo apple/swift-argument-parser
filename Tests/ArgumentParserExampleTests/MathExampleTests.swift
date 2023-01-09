99e2e97fd9fda9ce07e02e845ad53c5e072b0756
matsuji <mtj0928@users.noreply.github.com>
2023-01-10 01:55:41 +0900

    Improve fish completion (#376, #534) (#535)
    A generated fish script didn't complete an option after an argument.
    
    The cause is the generated fish script doesn't accept an input text which already has arguments.
    For example, when the input is `repeat -`, the script can complete `--count`, but when the text is `repeat foo -`, the script cannot complete `repeat foo --count`, because the input text already has the argument "foo".
    
    To fix the issue, `FishCompletionsGenerator` got a capability that can accept a text which has arguments.

--- Tests/ArgumentParserExampleTests/MathExampleTests.swift
+++ Tests/ArgumentParserExampleTests/MathExampleTests.swift
@@ -545,45 +545,65 @@ _math
 """
 
 private let fishCompletionScriptText = """
+# A function which filters options which starts with \"-\" from $argv.
+function _swift_math_preprocessor
+    set -l results
+    for i in (seq (count $argv))
+        switch (echo $argv[$i] | string sub -l 1)
+            case '-'
+            case '*'
+                echo $argv[$i]
+        end
+    end
+end
+
 function _swift_math_using_command
-    set -l cmd (commandline -opc)
-    if [ (count $cmd) -eq (count $argv) ]
-        for i in (seq (count $argv))
-            if [ $cmd[$i] != $argv[$i] ]
+    set -l currentCommands (_swift_math_preprocessor (commandline -opc))
+    set -l expectedCommands (string split \" \" $argv[1])
+    set -l subcommands (string split \" \" $argv[2])
+    if [ (count $currentCommands) -ge (count $expectedCommands) ]
+        for i in (seq (count $expectedCommands))
+            if [ $currentCommands[$i] != $expectedCommands[$i] ]
                 return 1
+            end
+        end
+        if [ (count $currentCommands) -eq (count $expectedCommands) ]
+            return 0
+        end
+        if [ (count $subcommands) -gt 1 ]
+            for i in (seq (count $subcommands))
+                if [ $currentCommands[(math (count $expectedCommands) + 1)] = $subcommands[$i] ]
+                    return 1
+                end
             end
         end
         return 0
     end
     return 1
 end
-complete -c math -n '_swift_math_using_command math' -f -l version -d 'Show the version.'
-complete -c math -n '_swift_math_using_command math' -f -s h -l help -d 'Show help information.'
-complete -c math -n '_swift_math_using_command math' -f -a 'add' -d 'Print the sum of the values.'
-complete -c math -n '_swift_math_using_command math' -f -a 'multiply' -d 'Print the product of the values.'
-complete -c math -n '_swift_math_using_command math' -f -a 'stats' -d 'Calculate descriptive statistics.'
-complete -c math -n '_swift_math_using_command math' -f -a 'help' -d 'Show subcommand help information.'
-complete -c math -n '_swift_math_using_command math add' -f -l hex-output -s x -d 'Use hexadecimal notation for the result.'
-complete -c math -n '_swift_math_using_command math add' -f -s h -l help -d 'Show help information.'
-complete -c math -n '_swift_math_using_command math multiply' -f -l hex-output -s x -d 'Use hexadecimal notation for the result.'
-complete -c math -n '_swift_math_using_command math multiply' -f -s h -l help -d 'Show help information.'
-complete -c math -n '_swift_math_using_command math stats' -f -s h -l help -d 'Show help information.'
-complete -c math -n '_swift_math_using_command math stats' -f -a 'average' -d 'Print the average of the values.'
-complete -c math -n '_swift_math_using_command math stats' -f -a 'stdev' -d 'Print the standard deviation of the values.'
-complete -c math -n '_swift_math_using_command math stats' -f -a 'quantiles' -d 'Print the quantiles of the values (TBD).'
-complete -c math -n '_swift_math_using_command math stats' -f -a 'help' -d 'Show subcommand help information.'
-complete -c math -n '_swift_math_using_command math stats average' -f -r -l kind -d 'The kind of average to provide.'
-complete -c math -n '_swift_math_using_command math stats average --kind' -f -k -a 'mean median mode'
-complete -c math -n '_swift_math_using_command math stats average' -f -l version -d 'Show the version.'
-complete -c math -n '_swift_math_using_command math stats average' -f -s h -l help -d 'Show help information.'
-complete -c math -n '_swift_math_using_command math stats stdev' -f -s h -l help -d 'Show help information.'
-complete -c math -n '_swift_math_using_command math stats quantiles' -f -r -l file
-complete -c math -n '_swift_math_using_command math stats quantiles --file' -f -a '(for i in *.{txt,md}; echo $i;end)'
-complete -c math -n '_swift_math_using_command math stats quantiles' -f -r -l directory
-complete -c math -n '_swift_math_using_command math stats quantiles --directory' -f -a '(__fish_complete_directories)'
-complete -c math -n '_swift_math_using_command math stats quantiles' -f -r -l shell
-complete -c math -n '_swift_math_using_command math stats quantiles --shell' -f -a '(head -100 /usr/share/dict/words | tail -50)'
-complete -c math -n '_swift_math_using_command math stats quantiles' -f -r -l custom
-complete -c math -n '_swift_math_using_command math stats quantiles --custom' -f -a '(command math ---completion stats quantiles -- --custom (commandline -opc)[1..-1])'
-complete -c math -n '_swift_math_using_command math stats quantiles' -f -s h -l help -d 'Show help information.'
+
+complete -c math -n \'_swift_math_using_command \"math add\"\' -l hex-output -s x -d \'Use hexadecimal notation for the result.\'
+complete -c math -n \'_swift_math_using_command \"math add\"\' -s h -l help -d \'Show help information.\'
+complete -c math -n \'_swift_math_using_command \"math multiply\"\' -l hex-output -s x -d \'Use hexadecimal notation for the result.\'
+complete -c math -n \'_swift_math_using_command \"math multiply\"\' -s h -l help -d \'Show help information.\'
+complete -c math -n \'_swift_math_using_command \"math stats average\"\' -l kind -d \'The kind of average to provide.\' -r -f -k -a \'mean median mode\'
+complete -c math -n \'_swift_math_using_command \"math stats average\"\' -l version -d \'Show the version.\'
+complete -c math -n \'_swift_math_using_command \"math stats average\"\' -s h -l help -d \'Show help information.\'
+complete -c math -n \'_swift_math_using_command \"math stats stdev\"\' -s h -l help -d \'Show help information.\'
+complete -c math -n \'_swift_math_using_command \"math stats quantiles\"\' -l file -r -f -a \'(for i in *.{txt,md}; echo $i;end)\'
+complete -c math -n \'_swift_math_using_command \"math stats quantiles\"\' -l directory -r -f -a \'(__fish_complete_directories)\'
+complete -c math -n \'_swift_math_using_command \"math stats quantiles\"\' -l shell -r -f -a \'(head -100 /usr/share/dict/words | tail -50)\'
+complete -c math -n \'_swift_math_using_command \"math stats quantiles\"\' -l custom -r -f -a \'(command math ---completion stats quantiles -- --custom (commandline -opc)[1..-1])\'
+complete -c math -n \'_swift_math_using_command \"math stats quantiles\"\' -s h -l help -d \'Show help information.\'
+complete -c math -n \'_swift_math_using_command \"math stats\" \"average stdev quantiles help\"\' -s h -l help -d \'Show help information.\'
+complete -c math -n \'_swift_math_using_command \"math stats\" \"average stdev quantiles help\"\' -f -a \'average\' -d \'Print the average of the values.\'
+complete -c math -n \'_swift_math_using_command \"math stats\" \"average stdev quantiles help\"\' -f -a \'stdev\' -d \'Print the standard deviation of the values.\'
+complete -c math -n \'_swift_math_using_command \"math stats\" \"average stdev quantiles help\"\' -f -a \'quantiles\' -d \'Print the quantiles of the values (TBD).\'
+complete -c math -n \'_swift_math_using_command \"math stats\" \"average stdev quantiles help\"\' -f -a \'help\' -d \'Show subcommand help information.\'
+complete -c math -n \'_swift_math_using_command \"math\" \"add multiply stats help\"\' -l version -d \'Show the version.\'
+complete -c math -n \'_swift_math_using_command \"math\" \"add multiply stats help\"\' -s h -l help -d \'Show help information.\'
+complete -c math -n \'_swift_math_using_command \"math\" \"add multiply stats help\"\' -f -a \'add\' -d \'Print the sum of the values.\'
+complete -c math -n \'_swift_math_using_command \"math\" \"add multiply stats help\"\' -f -a \'multiply\' -d \'Print the product of the values.\'
+complete -c math -n \'_swift_math_using_command \"math\" \"add multiply stats help\"\' -f -a \'stats\' -d \'Calculate descriptive statistics.\'
+complete -c math -n \'_swift_math_using_command \"math\" \"add multiply stats help\"\' -f -a \'help\' -d \'Show subcommand help information.\'
 """
