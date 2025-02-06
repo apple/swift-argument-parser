8b8bc61b335f4a7d9e6ff1f96693003e6598280b
Rauhul Varma <rauhul@apple.com>
2025-02-06 10:23:49 -0800

    Move additional tests to snapshots
    Moves some examples test to use snapshot files instead of inline
    multiline strings.

---
+++ Tests/ArgumentParserExampleTests/Snapshots/testMathBashCompletionScript().bash
@@ -0,0 +1,151 @@
+#!/bin/bash
+
+_math() {
+    export SAP_SHELL=bash
+    SAP_SHELL_VERSION="$(IFS='.'; printf %s "${BASH_VERSINFO[*]}")"
+    export SAP_SHELL_VERSION
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    prev="${COMP_WORDS[COMP_CWORD-1]}"
+    COMPREPLY=()
+    opts="--version -h --help add multiply stats help"
+    if [[ $COMP_CWORD == "1" ]]; then
+        COMPREPLY=( $(compgen -W "$opts" -- "$cur") )
+        return
+    fi
+    case ${COMP_WORDS[1]} in
+        (add)
+            _math_add 2
+            return
+            ;;
+        (multiply)
+            _math_multiply 2
+            return
+            ;;
+        (stats)
+            _math_stats 2
+            return
+            ;;
+        (help)
+            _math_help 2
+            return
+            ;;
+    esac
+    COMPREPLY=( $(compgen -W "$opts" -- "$cur") )
+}
+_math_add() {
+    opts="--hex-output -x --version -h --help"
+    if [[ $COMP_CWORD == "$1" ]]; then
+        COMPREPLY=( $(compgen -W "$opts" -- "$cur") )
+        return
+    fi
+    COMPREPLY=( $(compgen -W "$opts" -- "$cur") )
+}
+_math_multiply() {
+    opts="--hex-output -x --version -h --help"
+    if [[ $COMP_CWORD == "$1" ]]; then
+        COMPREPLY=( $(compgen -W "$opts" -- "$cur") )
+        return
+    fi
+    COMPREPLY=( $(compgen -W "$opts" -- "$cur") )
+}
+_math_stats() {
+    opts="--version -h --help average stdev quantiles"
+    if [[ $COMP_CWORD == "$1" ]]; then
+        COMPREPLY=( $(compgen -W "$opts" -- "$cur") )
+        return
+    fi
+    case ${COMP_WORDS[$1]} in
+        (average)
+            _math_stats_average $(($1+1))
+            return
+            ;;
+        (stdev)
+            _math_stats_stdev $(($1+1))
+            return
+            ;;
+        (quantiles)
+            _math_stats_quantiles $(($1+1))
+            return
+            ;;
+    esac
+    COMPREPLY=( $(compgen -W "$opts" -- "$cur") )
+}
+_math_stats_average() {
+    opts="--kind --version -h --help"
+    if [[ $COMP_CWORD == "$1" ]]; then
+        COMPREPLY=( $(compgen -W "$opts" -- "$cur") )
+        return
+    fi
+    case $prev in
+        --kind)
+            COMPREPLY=( $(compgen -W "mean median mode" -- "$cur") )
+            return
+        ;;
+    esac
+    COMPREPLY=( $(compgen -W "$opts" -- "$cur") )
+}
+_math_stats_stdev() {
+    opts="--version -h --help"
+    if [[ $COMP_CWORD == "$1" ]]; then
+        COMPREPLY=( $(compgen -W "$opts" -- "$cur") )
+        return
+    fi
+    COMPREPLY=( $(compgen -W "$opts" -- "$cur") )
+}
+_math_stats_quantiles() {
+    opts="--file --directory --shell --custom --version -h --help"
+    opts="$opts alphabet alligator branch braggart"
+    opts="$opts $("${COMP_WORDS[0]}" ---completion stats quantiles -- customArg "${COMP_WORDS[@]}")"
+    if [[ $COMP_CWORD == "$1" ]]; then
+        COMPREPLY=( $(compgen -W "$opts" -- "$cur") )
+        return
+    fi
+    case $prev in
+        --file)
+            if declare -F _filedir >/dev/null; then
+              _filedir 'txt'
+              _filedir 'md'
+              _filedir 'TXT'
+              _filedir 'MD'
+              _filedir -d
+            else
+              COMPREPLY=(
+                $(compgen -f -X '!*.txt' -- "$cur")
+                $(compgen -f -X '!*.md' -- "$cur")
+                $(compgen -f -X '!*.TXT' -- "$cur")
+                $(compgen -f -X '!*.MD' -- "$cur")
+                $(compgen -d -- "$cur")
+              )
+            fi
+            return
+        ;;
+        --directory)
+            if declare -F _filedir >/dev/null; then
+              _filedir -d
+            else
+              COMPREPLY=( $(compgen -d -- "$cur") )
+            fi
+            return
+        ;;
+        --shell)
+            COMPREPLY=( $(head -100 /usr/share/dict/words | tail -50) )
+            return
+        ;;
+        --custom)
+            COMPREPLY=( $(compgen -W "$("${COMP_WORDS[0]}" ---completion stats quantiles -- --custom "${COMP_WORDS[@]}")" -- "$cur") )
+            return
+        ;;
+    esac
+    COMPREPLY=( $(compgen -W "$opts" -- "$cur") )
+}
+_math_help() {
+    opts="--version"
+    if [[ $COMP_CWORD == "$1" ]]; then
+        COMPREPLY=( $(compgen -W "$opts" -- "$cur") )
+        return
+    fi
+    COMPREPLY=( $(compgen -W "$opts" -- "$cur") )
+}
+
+
+complete -F _math math
---
+++ Tests/ArgumentParserExampleTests/Snapshots/testMathZshCompletionScript().zsh
@@ -0,0 +1,181 @@
+#compdef math
+local context state state_descr line
+_math_commandname=$words[1]
+typeset -A opt_args
+
+_math() {
+    export SAP_SHELL=zsh
+    SAP_SHELL_VERSION="$(builtin emulate zsh -c 'printf %s "${ZSH_VERSION}"')"
+    export SAP_SHELL_VERSION
+    integer ret=1
+    local -a args
+    args+=(
+        '--version[Show the version.]'
+        '(-h --help)'{-h,--help}'[Show help information.]'
+        '(-): :->command'
+        '(-)*:: :->arg'
+    )
+    _arguments -w -s -S $args[@] && ret=0
+    case $state in
+        (command)
+            local subcommands
+            subcommands=(
+                'add:Print the sum of the values.'
+                'multiply:Print the product of the values.'
+                'stats:Calculate descriptive statistics.'
+                'help:Show subcommand help information.'
+            )
+            _describe "subcommand" subcommands
+            ;;
+        (arg)
+            case ${words[1]} in
+                (add)
+                    _math_add
+                    ;;
+                (multiply)
+                    _math_multiply
+                    ;;
+                (stats)
+                    _math_stats
+                    ;;
+                (help)
+                    _math_help
+                    ;;
+            esac
+            ;;
+    esac
+
+    return ret
+}
+
+_math_add() {
+    integer ret=1
+    local -a args
+    args+=(
+        '(--hex-output -x)'{--hex-output,-x}'[Use hexadecimal notation for the result.]'
+        ':values:'
+        '--version[Show the version.]'
+        '(-h --help)'{-h,--help}'[Show help information.]'
+    )
+    _arguments -w -s -S $args[@] && ret=0
+
+    return ret
+}
+
+_math_multiply() {
+    integer ret=1
+    local -a args
+    args+=(
+        '(--hex-output -x)'{--hex-output,-x}'[Use hexadecimal notation for the result.]'
+        ':values:'
+        '--version[Show the version.]'
+        '(-h --help)'{-h,--help}'[Show help information.]'
+    )
+    _arguments -w -s -S $args[@] && ret=0
+
+    return ret
+}
+
+_math_stats() {
+    integer ret=1
+    local -a args
+    args+=(
+        '--version[Show the version.]'
+        '(-h --help)'{-h,--help}'[Show help information.]'
+        '(-): :->command'
+        '(-)*:: :->arg'
+    )
+    _arguments -w -s -S $args[@] && ret=0
+    case $state in
+        (command)
+            local subcommands
+            subcommands=(
+                'average:Print the average of the values.'
+                'stdev:Print the standard deviation of the values.'
+                'quantiles:Print the quantiles of the values (TBD).'
+            )
+            _describe "subcommand" subcommands
+            ;;
+        (arg)
+            case ${words[1]} in
+                (average)
+                    _math_stats_average
+                    ;;
+                (stdev)
+                    _math_stats_stdev
+                    ;;
+                (quantiles)
+                    _math_stats_quantiles
+                    ;;
+            esac
+            ;;
+    esac
+
+    return ret
+}
+
+_math_stats_average() {
+    integer ret=1
+    local -a args
+    args+=(
+        '--kind[The kind of average to provide.]:kind:(mean median mode)'
+        ':values:'
+        '--version[Show the version.]'
+        '(-h --help)'{-h,--help}'[Show help information.]'
+    )
+    _arguments -w -s -S $args[@] && ret=0
+
+    return ret
+}
+
+_math_stats_stdev() {
+    integer ret=1
+    local -a args
+    args+=(
+        ':values:'
+        '--version[Show the version.]'
+        '(-h --help)'{-h,--help}'[Show help information.]'
+    )
+    _arguments -w -s -S $args[@] && ret=0
+
+    return ret
+}
+
+_math_stats_quantiles() {
+    integer ret=1
+    local -a args
+    args+=(
+        ':one-of-four:(alphabet alligator branch braggart)'
+        ':custom-arg:{_custom_completion $_math_commandname ---completion stats quantiles -- customArg $words}'
+        ':values:'
+        '--file:file:_files -g '"'"'*.txt *.md'"'"''
+        '--directory:directory:_files -/'
+        '--shell:shell:{local -a list; list=(${(f)"$(head -100 /usr/share/dict/words | tail -50)"}); _describe '''' list}'
+        '--custom:custom:{_custom_completion $_math_commandname ---completion stats quantiles -- --custom $words}'
+        '--version[Show the version.]'
+        '(-h --help)'{-h,--help}'[Show help information.]'
+    )
+    _arguments -w -s -S $args[@] && ret=0
+
+    return ret
+}
+
+_math_help() {
+    integer ret=1
+    local -a args
+    args+=(
+        ':subcommands:'
+        '--version[Show the version.]'
+    )
+    _arguments -w -s -S $args[@] && ret=0
+
+    return ret
+}
+
+
+_custom_completion() {
+    local completions=("${(@f)$($*)}")
+    _describe '' completions
+}
+
+_math
