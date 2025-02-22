964da9f2909b0047dfbb0c16c023734a620e52b4
Nate Cook <natecook@apple.com>
2025-02-22 15:34:31 -0600

    Improve output when validation fails
    This also revamps the file structure for validators.

---
+++ Sources/ArgumentParser/Validators/ParsableArgumentsValidation.swift
@@ -0,0 +1,66 @@
+//===----------------------------------------------------------*- swift -*-===//
+//
+// This source file is part of the Swift Argument Parser open source project
+//
+// Copyright (c) 2020 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+//
+//===----------------------------------------------------------------------===//
+
+extension ParsableArguments {
+  static func _validate(parent: InputKey?) throws {
+    let validators: [ParsableArgumentsValidator.Type] = [
+      PositionalArgumentsValidator.self,
+      CodingKeyValidator.self,
+      UniqueNamesValidator.self,
+      NonsenseFlagsValidator.self,
+    ]
+    let errors = validators.compactMap { validator in
+      validator.validate(self, parent: parent)
+    }
+    if errors.count > 0 {
+      throw ParsableArgumentsValidationError(
+        parsableArgumentsType: self, underlayingErrors: errors)
+    }
+  }
+}
+
+protocol ParsableArgumentsValidator {
+  static func validate(_ type: ParsableArguments.Type, parent: InputKey?)
+    -> ParsableArgumentsValidatorError?
+}
+
+enum ValidatorErrorKind {
+  case warning
+  case failure
+}
+
+protocol ParsableArgumentsValidatorError: Error {
+  var kind: ValidatorErrorKind { get }
+}
+
+struct ParsableArgumentsValidationError: Error, CustomStringConvertible {
+  let parsableArgumentsType: ParsableArguments.Type
+  let underlayingErrors: [Error]
+
+  var description: String {
+    let errorDescriptions =
+      underlayingErrors
+      .map {
+        "- \($0)"
+          .wrapped(to: 68)
+          .hangingIndentingEachLine(by: 2)
+      }
+    return """
+
+      --------------------------------------------------------------------
+      Validation failed for `\(parsableArgumentsType)`:
+
+      \(errorDescriptions.joined(separator: "\n"))
+      --------------------------------------------------------------------
+
+      """
+  }
+}
---
+++ Sources/ArgumentParser/Validators/UniqueNamesValidator.swift
@@ -0,0 +1,57 @@
+//===----------------------------------------------------------*- swift -*-===//
+//
+// This source file is part of the Swift Argument Parser open source project
+//
+// Copyright (c) 2020 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+//
+//===----------------------------------------------------------------------===//
+
+/// A validator that ensures argument names are unique within a
+/// `ParsableArguments` or `ParsableCommand`.
+struct UniqueNamesValidator: ParsableArgumentsValidator {
+  struct Error: ParsableArgumentsValidatorError, CustomStringConvertible {
+    var duplicateNames: [String: Int] = [:]
+
+    var description: String {
+      duplicateNames.map { entry in
+        """
+        Multiple (\(entry.value)) `Option` or `Flag` arguments are named \
+        "\(entry.key)".
+        """
+      }.joined(separator: "\n")
+    }
+
+    var kind: ValidatorErrorKind { .failure }
+  }
+
+  static func validate(_ type: ParsableArguments.Type, parent: InputKey?)
+    -> ParsableArgumentsValidatorError?
+  {
+    let argSets: [ArgumentSet] = Mirror(reflecting: type.init())
+      .children
+      .compactMap { child in
+        guard
+          let codingKey = child.label,
+          let parsed = child.value as? ArgumentSetProvider
+        else { return nil }
+
+        let key = InputKey(name: codingKey, parent: parent)
+        return parsed.argumentSet(for: key)
+      }
+
+    let countedNames: [String: Int] = argSets.reduce(into: [:]) {
+      countedNames, args in
+      for name in args.content.flatMap({ $0.names }) {
+        countedNames[name.synopsisString, default: 0] += 1
+      }
+    }
+
+    let duplicateNames = countedNames.filter { $0.value > 1 }
+    return duplicateNames.isEmpty
+      ? nil
+      : Error(duplicateNames: duplicateNames)
+  }
+}
