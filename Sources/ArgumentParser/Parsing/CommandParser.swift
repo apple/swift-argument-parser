75194a48a15c9bd44bd0218594ed5613470a8a4c
Ross Goldberg <484615+rgoldberg@users.noreply.github.com>
2025-05-13 00:00:31 -0400

    Replace the cursor index within the completing word parameter of custom-completion closures with a `String` `completionPrefix` parameter.
    Signed-off-by: Ross Goldberg <484615+rgoldberg@users.noreply.github.com>

--- Sources/ArgumentParser/Parsing/CommandParser.swift
+++ Sources/ArgumentParser/Parsing/CommandParser.swift
@@ -456,16 +456,32 @@ extension CommandParser {
       }
 
       guard
-        let s = args.popFirst(),
-        let cursorIndexWithinCompletingArgument = Int(s)
+        let arg = args.popFirst(),
+        let cursorIndexWithinCompletingArgument = Int(arg)
       else {
+        throw ParserError.invalidState
+      }
+
+      let completingPrefix: String
+      if let completingArgument = args.last {
+        completingPrefix = String(
+          completingArgument.prefix(
+            upTo: completingArgument.index(
+              completingArgument.startIndex,
+              offsetBy: cursorIndexWithinCompletingArgument
+            )
+          )
+        )
+      } else if cursorIndexWithinCompletingArgument == 0 {
+        completingPrefix = ""
+      } else {
         throw ParserError.invalidState
       }
 
       completions = complete(
         Array(args),
         completingArgumentIndex,
-        cursorIndexWithinCompletingArgument
+        completingPrefix
       )
     case .customDeprecated(let complete):
       completions = complete(args)
