4a6db87cb841be1889160921a4ed312093446362
Chris McGee <christie_mcgee@apple.com>
2025-08-16 13:17:24 -0400

    Restore parsable arguments file to previous state

--- Sources/ArgumentParser/Parsable Types/ParsableArguments.swift
+++ Sources/ArgumentParser/Parsable Types/ParsableArguments.swift
@@ -74,8 +74,7 @@ extension ParsableArguments {
     _ arguments: [String]? = nil
   ) throws -> Self {
     // Parse the command and unwrap the result if necessary.
-    let cmd = try self.asCommand.parseAsRoot(arguments)
-    switch cmd {
+    switch try self.asCommand.parseAsRoot(arguments) {
     case let helpCommand as HelpCommand:
       throw ParserError.helpRequested(visibility: helpCommand.visibility)
     case let result as _WrappedParsableCommand<Self>:
