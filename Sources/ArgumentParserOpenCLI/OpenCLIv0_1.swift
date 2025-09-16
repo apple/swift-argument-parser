5fa67f02bd7406e1fb3c46d1bc4d194cc12ec598
Chris McGee <cmcgee1024@gmail.com>
2025-09-16 11:38:58 -0400

    Fix license check

--- Sources/ArgumentParserOpenCLI/OpenCLIv0_1.swift
+++ Sources/ArgumentParserOpenCLI/OpenCLIv0_1.swift
@@ -1,3 +1,14 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift Argument Parser open source project
+//
+// Copyright (c) 2025 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+//
+//===----------------------------------------------------------------------===//
+
 public struct OpenCLIv0_1: Codable, Equatable {
   public let opencli: String
   public let info: CliInfo
