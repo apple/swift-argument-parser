6d473eaa0ab01ac255c0864acf24df377ff246c9
Ross Goldberg <484615+rgoldberg@users.noreply.github.com>
2025-12-17 09:41:19 -0500

    Fix generated shell completion script handling of repeating & non-repeating positional arguments, flags & options (#808)
    * Complete repeatable positional instances after the first in bash.
    * Complete repeatable positional instances after the first in fish.
    * Only complete positionals through the first repeating positional in zsh.
    * Complete repeatable flag & option instances after the first in bash.
    * Complete non-repeatable flag & option instances only once in fish.

--- Tests/ArgumentParserExampleTests/Snapshots/testMathFishCompletionScript().fish
+++ Tests/ArgumentParserExampleTests/Snapshots/testMathFishCompletionScript().fish
@@ -1,7 +1,29 @@
-function __math_should_offer_completions_for -a expected_commands -a expected_positional_index
-    set -l unparsed_tokens (__math_tokens -pc)
+function __math_should_offer_completions_for_flags_or_options -a expected_commands
+    set -l non_repeating_flags_or_options $argv[2..]
+
+    set -l non_repeating_flags_or_options_absent 0
     set -l positional_index 0
     set -l commands
+    __math_parse_tokens
+    test "$commands" = "$expected_commands"; and return $non_repeating_flags_or_options_absent
+end
+
+function __math_should_offer_completions_for_positional -a expected_commands expected_positional_index positional_index_comparison
+    if test -z $positional_index_comparison
+        set positional_index_comparison -eq
+    end
+
+    set -l non_repeating_flags_or_options
+    set -l non_repeating_flags_or_options_absent 0
+    set -l positional_index 0
+    set -l commands
+    __math_parse_tokens
+    test "$commands" = "$expected_commands" -a \( "$positional_index" "$positional_index_comparison" "$expected_positional_index" \)
+end
+
+function __math_parse_tokens -S
+    set -l unparsed_tokens (__math_tokens -pc)
+    set -l present_flags_and_options
 
     switch $unparsed_tokens[1]
     case 'math'
@@ -25,8 +47,6 @@ function __math_should_offer_completions_for -a expected_commands -a expected_po
             __math_parse_subcommand -r 1 'version'
         end
     end
-
-    test "$commands" = "$expected_commands" -a \( -z "$expected_positional_index" -o "$expected_positional_index" -eq "$positional_index" \)
 end
 
 function __math_tokens
@@ -37,9 +57,8 @@ function __math_tokens
     end
 end
 
-function __math_parse_subcommand -S
+function __math_parse_subcommand -S -a positional_count
     argparse -s r -- $argv
-    set -l positional_count $argv[1]
     set -l option_specs $argv[2..]
 
     set -a commands $unparsed_tokens[1]
@@ -51,8 +70,16 @@ function __math_parse_subcommand -S
         argparse -sn "$commands" $option_specs -- $unparsed_tokens 2> /dev/null
         set unparsed_tokens $argv
         set positional_index (math $positional_index + 1)
+
+        for non_repeating_flag_or_option in $non_repeating_flags_or_options
+            if set -ql _flag_$non_repeating_flag_or_option
+                set non_repeating_flags_or_options_absent 1
+                break
+            end
+        end
+
         if test (count $unparsed_tokens) -eq 0 -o \( -z "$_flag_r" -a "$positional_index" -gt "$positional_count" \)
-            return 0
+            break
         end
         set -e unparsed_tokens[1]
     end
@@ -78,36 +105,36 @@ function __math_custom_completion
 end
 
 complete -c 'math' -f
-complete -c 'math' -n '__math_should_offer_completions_for "math"' -l 'version' -d 'Show the version.'
-complete -c 'math' -n '__math_should_offer_completions_for "math"' -s 'h' -l 'help' -d 'Show help information.'
-complete -c 'math' -n '__math_should_offer_completions_for "math" 1' -fa 'add' -d 'Print the sum of the values.'
-complete -c 'math' -n '__math_should_offer_completions_for "math" 1' -fa 'multiply' -d 'Print the product of the values.'
-complete -c 'math' -n '__math_should_offer_completions_for "math" 1' -fa 'stats' -d 'Calculate descriptive statistics.'
-complete -c 'math' -n '__math_should_offer_completions_for "math" 1' -fa 'help' -d 'Show subcommand help information.'
-complete -c 'math' -n '__math_should_offer_completions_for "math add"' -l 'hex-output' -s 'x' -d 'Use hexadecimal notation for the result.'
-complete -c 'math' -n '__math_should_offer_completions_for "math add"' -l 'version' -d 'Show the version.'
-complete -c 'math' -n '__math_should_offer_completions_for "math add"' -s 'h' -l 'help' -d 'Show help information.'
-complete -c 'math' -n '__math_should_offer_completions_for "math multiply"' -l 'hex-output' -s 'x' -d 'Use hexadecimal notation for the result.'
-complete -c 'math' -n '__math_should_offer_completions_for "math multiply"' -l 'version' -d 'Show the version.'
-complete -c 'math' -n '__math_should_offer_completions_for "math multiply"' -s 'h' -l 'help' -d 'Show help information.'
-complete -c 'math' -n '__math_should_offer_completions_for "math stats"' -l 'version' -d 'Show the version.'
-complete -c 'math' -n '__math_should_offer_completions_for "math stats"' -s 'h' -l 'help' -d 'Show help information.'
-complete -c 'math' -n '__math_should_offer_completions_for "math stats" 1' -fa 'average' -d 'Print the average of the values.'
-complete -c 'math' -n '__math_should_offer_completions_for "math stats" 1' -fa 'stdev' -d 'Print the standard deviation of the values.'
-complete -c 'math' -n '__math_should_offer_completions_for "math stats" 1' -fa 'quantiles' -d 'Print the quantiles of the values (TBD).'
-complete -c 'math' -n '__math_should_offer_completions_for "math stats average"' -l 'kind' -d 'The kind of average to provide.' -rfka 'mean median mode'
-complete -c 'math' -n '__math_should_offer_completions_for "math stats average"' -l 'version' -d 'Show the version.'
-complete -c 'math' -n '__math_should_offer_completions_for "math stats average"' -s 'h' -l 'help' -d 'Show help information.'
-complete -c 'math' -n '__math_should_offer_completions_for "math stats stdev"' -l 'version' -d 'Show the version.'
-complete -c 'math' -n '__math_should_offer_completions_for "math stats stdev"' -s 'h' -l 'help' -d 'Show help information.'
-complete -c 'math' -n '__math_should_offer_completions_for "math stats quantiles" 1' -fka 'alphabet alligator branch braggart'
-complete -c 'math' -n '__math_should_offer_completions_for "math stats quantiles" 2' -fka '(__math_custom_completion ---completion stats quantiles -- positional@1 (count (__math_tokens -pc)) (__math_tokens -tC))'
-complete -c 'math' -n '__math_should_offer_completions_for "math stats quantiles" 3' -fka '(__math_custom_completion ---completion stats quantiles -- positional@2)'
-complete -c 'math' -n '__math_should_offer_completions_for "math stats quantiles"' -l 'file' -rfa '(set -l exts \'txt\' \'md\';for p in (string match -e -- \'*/\' (commandline -t);or printf \n)*.{$exts};printf %s\n $p;end;__fish_complete_directories (commandline -t) \'\')'
-complete -c 'math' -n '__math_should_offer_completions_for "math stats quantiles"' -l 'directory' -rfa '(__math_complete_directories)'
-complete -c 'math' -n '__math_should_offer_completions_for "math stats quantiles"' -l 'shell' -rfka '(head -100 \'/usr/share/dict/words\' | tail -50)'
-complete -c 'math' -n '__math_should_offer_completions_for "math stats quantiles"' -l 'custom' -rfka '(__math_custom_completion ---completion stats quantiles -- --custom (count (__math_tokens -pc)) (__math_tokens -tC))'
-complete -c 'math' -n '__math_should_offer_completions_for "math stats quantiles"' -l 'custom-deprecated' -rfka '(__math_custom_completion ---completion stats quantiles -- --custom-deprecated)'
-complete -c 'math' -n '__math_should_offer_completions_for "math stats quantiles"' -l 'version' -d 'Show the version.'
-complete -c 'math' -n '__math_should_offer_completions_for "math stats quantiles"' -s 'h' -l 'help' -d 'Show help information.'
-complete -c 'math' -n '__math_should_offer_completions_for "math help"' -l 'version' -d 'Show the version.'
+complete -c 'math' -n '__math_should_offer_completions_for_flags_or_options "math" version' -l 'version' -d 'Show the version.'
+complete -c 'math' -n '__math_should_offer_completions_for_flags_or_options "math" h help' -s 'h' -l 'help' -d 'Show help information.'
+complete -c 'math' -n '__math_should_offer_completions_for_positional "math" 1' -fa 'add' -d 'Print the sum of the values.'
+complete -c 'math' -n '__math_should_offer_completions_for_positional "math" 1' -fa 'multiply' -d 'Print the product of the values.'
+complete -c 'math' -n '__math_should_offer_completions_for_positional "math" 1' -fa 'stats' -d 'Calculate descriptive statistics.'
+complete -c 'math' -n '__math_should_offer_completions_for_positional "math" 1' -fa 'help' -d 'Show subcommand help information.'
+complete -c 'math' -n '__math_should_offer_completions_for_flags_or_options "math add" hex-output x' -l 'hex-output' -s 'x' -d 'Use hexadecimal notation for the result.'
+complete -c 'math' -n '__math_should_offer_completions_for_flags_or_options "math add" version' -l 'version' -d 'Show the version.'
+complete -c 'math' -n '__math_should_offer_completions_for_flags_or_options "math add" h help' -s 'h' -l 'help' -d 'Show help information.'
+complete -c 'math' -n '__math_should_offer_completions_for_flags_or_options "math multiply" hex-output x' -l 'hex-output' -s 'x' -d 'Use hexadecimal notation for the result.'
+complete -c 'math' -n '__math_should_offer_completions_for_flags_or_options "math multiply" version' -l 'version' -d 'Show the version.'
+complete -c 'math' -n '__math_should_offer_completions_for_flags_or_options "math multiply" h help' -s 'h' -l 'help' -d 'Show help information.'
+complete -c 'math' -n '__math_should_offer_completions_for_flags_or_options "math stats" version' -l 'version' -d 'Show the version.'
+complete -c 'math' -n '__math_should_offer_completions_for_flags_or_options "math stats" h help' -s 'h' -l 'help' -d 'Show help information.'
+complete -c 'math' -n '__math_should_offer_completions_for_positional "math stats" 1' -fa 'average' -d 'Print the average of the values.'
+complete -c 'math' -n '__math_should_offer_completions_for_positional "math stats" 1' -fa 'stdev' -d 'Print the standard deviation of the values.'
+complete -c 'math' -n '__math_should_offer_completions_for_positional "math stats" 1' -fa 'quantiles' -d 'Print the quantiles of the values (TBD).'
+complete -c 'math' -n '__math_should_offer_completions_for_flags_or_options "math stats average" kind' -l 'kind' -d 'The kind of average to provide.' -rfka 'mean median mode'
+complete -c 'math' -n '__math_should_offer_completions_for_flags_or_options "math stats average" version' -l 'version' -d 'Show the version.'
+complete -c 'math' -n '__math_should_offer_completions_for_flags_or_options "math stats average" h help' -s 'h' -l 'help' -d 'Show help information.'
+complete -c 'math' -n '__math_should_offer_completions_for_flags_or_options "math stats stdev" version' -l 'version' -d 'Show the version.'
+complete -c 'math' -n '__math_should_offer_completions_for_flags_or_options "math stats stdev" h help' -s 'h' -l 'help' -d 'Show help information.'
+complete -c 'math' -n '__math_should_offer_completions_for_positional "math stats quantiles" 1' -fka 'alphabet alligator branch braggart'
+complete -c 'math' -n '__math_should_offer_completions_for_positional "math stats quantiles" 2' -fka '(__math_custom_completion ---completion stats quantiles -- positional@1 (count (__math_tokens -pc)) (__math_tokens -tC))'
+complete -c 'math' -n '__math_should_offer_completions_for_positional "math stats quantiles" 3' -fka '(__math_custom_completion ---completion stats quantiles -- positional@2)'
+complete -c 'math' -n '__math_should_offer_completions_for_flags_or_options "math stats quantiles" file' -l 'file' -rfa '(set -l exts \'txt\' \'md\';for p in (string match -e -- \'*/\' (commandline -t);or printf \n)*.{$exts};printf %s\n $p;end;__fish_complete_directories (commandline -t) \'\')'
+complete -c 'math' -n '__math_should_offer_completions_for_flags_or_options "math stats quantiles" directory' -l 'directory' -rfa '(__math_complete_directories)'
+complete -c 'math' -n '__math_should_offer_completions_for_flags_or_options "math stats quantiles" shell' -l 'shell' -rfka '(head -100 \'/usr/share/dict/words\' | tail -50)'
+complete -c 'math' -n '__math_should_offer_completions_for_flags_or_options "math stats quantiles" custom' -l 'custom' -rfka '(__math_custom_completion ---completion stats quantiles -- --custom (count (__math_tokens -pc)) (__math_tokens -tC))'
+complete -c 'math' -n '__math_should_offer_completions_for_flags_or_options "math stats quantiles" custom-deprecated' -l 'custom-deprecated' -rfka '(__math_custom_completion ---completion stats quantiles -- --custom-deprecated)'
+complete -c 'math' -n '__math_should_offer_completions_for_flags_or_options "math stats quantiles" version' -l 'version' -d 'Show the version.'
+complete -c 'math' -n '__math_should_offer_completions_for_flags_or_options "math stats quantiles" h help' -s 'h' -l 'help' -d 'Show help information.'
+complete -c 'math' -n '__math_should_offer_completions_for_flags_or_options "math help" version' -l 'version' -d 'Show the version.'
--- Tests/ArgumentParserUnitTests/Snapshots/testBase_Fish().fish
+++ Tests/ArgumentParserUnitTests/Snapshots/testBase_Fish().fish
@@ -1,11 +1,33 @@
-function __base-test_should_offer_completions_for -a expected_commands -a expected_positional_index
-    set -l unparsed_tokens (__base-test_tokens -pc)
+function __base-test_should_offer_completions_for_flags_or_options -a expected_commands
+    set -l non_repeating_flags_or_options $argv[2..]
+
+    set -l non_repeating_flags_or_options_absent 0
     set -l positional_index 0
     set -l commands
+    __base-test_parse_tokens
+    test "$commands" = "$expected_commands"; and return $non_repeating_flags_or_options_absent
+end
+
+function __base-test_should_offer_completions_for_positional -a expected_commands expected_positional_index positional_index_comparison
+    if test -z $positional_index_comparison
+        set positional_index_comparison -eq
+    end
+
+    set -l non_repeating_flags_or_options
+    set -l non_repeating_flags_or_options_absent 0
+    set -l positional_index 0
+    set -l commands
+    __base-test_parse_tokens
+    test "$commands" = "$expected_commands" -a \( "$positional_index" "$positional_index_comparison" "$expected_positional_index" \)
+end
+
+function __base-test_parse_tokens -S
+    set -l unparsed_tokens (__base-test_tokens -pc)
+    set -l present_flags_and_options
 
     switch $unparsed_tokens[1]
     case 'base-test'
-        __base-test_parse_subcommand 2 'name=' 'kind=' 'other-kind=' 'path1=' 'path2=' 'path3=' 'one' 'two' 'custom-three' 'kind-counter' 'rep1=' 'r/rep2=' 'h/help'
+        __base-test_parse_subcommand 2 'name=' 'kind=' 'other-kind=' 'path1=' 'path2=' 'path3=' 'one' 'two' 'custom-three' 'kind-counter' 'rep1=+' 'r/rep2=+' 'h/help'
         switch $unparsed_tokens[1]
         case 'sub-command'
             __base-test_parse_subcommand 0 'h/help'
@@ -15,8 +37,6 @@ function __base-test_should_offer_completions_for -a expected_commands -a expect
             __base-test_parse_subcommand -r 1 
         end
     end
-
-    test "$commands" = "$expected_commands" -a \( -z "$expected_positional_index" -o "$expected_positional_index" -eq "$positional_index" \)
 end
 
 function __base-test_tokens
@@ -27,9 +47,8 @@ function __base-test_tokens
     end
 end
 
-function __base-test_parse_subcommand -S
+function __base-test_parse_subcommand -S -a positional_count
     argparse -s r -- $argv
-    set -l positional_count $argv[1]
     set -l option_specs $argv[2..]
 
     set -a commands $unparsed_tokens[1]
@@ -41,8 +60,16 @@ function __base-test_parse_subcommand -S
         argparse -sn "$commands" $option_specs -- $unparsed_tokens 2> /dev/null
         set unparsed_tokens $argv
         set positional_index (math $positional_index + 1)
+
+        for non_repeating_flag_or_option in $non_repeating_flags_or_options
+            if set -ql _flag_$non_repeating_flag_or_option
+                set non_repeating_flags_or_options_absent 1
+                break
+            end
+        end
+
         if test (count $unparsed_tokens) -eq 0 -o \( -z "$_flag_r" -a "$positional_index" -gt "$positional_count" \)
-            return 0
+            break
         end
         set -e unparsed_tokens[1]
     end
@@ -68,25 +95,25 @@ function __base-test_custom_completion
 end
 
 complete -c 'base-test' -f
-complete -c 'base-test' -n '__base-test_should_offer_completions_for "base-test"' -l 'name' -d 'The user\'s name.' -rfka ''
-complete -c 'base-test' -n '__base-test_should_offer_completions_for "base-test"' -l 'kind' -rfka 'one two custom-three'
-complete -c 'base-test' -n '__base-test_should_offer_completions_for "base-test"' -l 'other-kind' -rfka 'b1_fish b2_fish b3_fish'
-complete -c 'base-test' -n '__base-test_should_offer_completions_for "base-test"' -l 'path1' -rF
-complete -c 'base-test' -n '__base-test_should_offer_completions_for "base-test"' -l 'path2' -rF
-complete -c 'base-test' -n '__base-test_should_offer_completions_for "base-test"' -l 'path3' -rfka 'c1_fish c2_fish c3_fish'
-complete -c 'base-test' -n '__base-test_should_offer_completions_for "base-test"' -l 'one'
-complete -c 'base-test' -n '__base-test_should_offer_completions_for "base-test"' -l 'two'
-complete -c 'base-test' -n '__base-test_should_offer_completions_for "base-test"' -l 'custom-three'
-complete -c 'base-test' -n '__base-test_should_offer_completions_for "base-test"' -l 'kind-counter'
-complete -c 'base-test' -n '__base-test_should_offer_completions_for "base-test"' -l 'rep1' -rfka ''
-complete -c 'base-test' -n '__base-test_should_offer_completions_for "base-test"' -s 'r' -l 'rep2' -rfka ''
-complete -c 'base-test' -n '__base-test_should_offer_completions_for "base-test" 1' -fka '(__base-test_custom_completion ---completion -- positional@0 (count (__base-test_tokens -pc)) (__base-test_tokens -tC))'
-complete -c 'base-test' -n '__base-test_should_offer_completions_for "base-test" 2' -fka '(__base-test_custom_completion ---completion -- positional@1 (count (__base-test_tokens -pc)) (__base-test_tokens -tC))'
-complete -c 'base-test' -n '__base-test_should_offer_completions_for "base-test"' -s 'h' -l 'help' -d 'Show help information.'
-complete -c 'base-test' -n '__base-test_should_offer_completions_for "base-test" 3' -fa 'sub-command' -d ''
-complete -c 'base-test' -n '__base-test_should_offer_completions_for "base-test" 3' -fa 'escaped-command' -d ''
-complete -c 'base-test' -n '__base-test_should_offer_completions_for "base-test" 3' -fa 'help' -d 'Show subcommand help information.'
-complete -c 'base-test' -n '__base-test_should_offer_completions_for "base-test sub-command"' -s 'h' -l 'help' -d 'Show help information.'
-complete -c 'base-test' -n '__base-test_should_offer_completions_for "base-test escaped-command"' -l 'o:n[e' -d 'Escaped chars: \'[]\\.' -rfka ''
-complete -c 'base-test' -n '__base-test_should_offer_completions_for "base-test escaped-command" 1' -fka '(__base-test_custom_completion ---completion escaped-command -- positional@0 (count (__base-test_tokens -pc)) (__base-test_tokens -tC))'
-complete -c 'base-test' -n '__base-test_should_offer_completions_for "base-test escaped-command"' -s 'h' -l 'help' -d 'Show help information.'
+complete -c 'base-test' -n '__base-test_should_offer_completions_for_flags_or_options "base-test" name' -l 'name' -d 'The user\'s name.' -rfka ''
+complete -c 'base-test' -n '__base-test_should_offer_completions_for_flags_or_options "base-test" kind' -l 'kind' -rfka 'one two custom-three'
+complete -c 'base-test' -n '__base-test_should_offer_completions_for_flags_or_options "base-test" other-kind' -l 'other-kind' -rfka 'b1_fish b2_fish b3_fish'
+complete -c 'base-test' -n '__base-test_should_offer_completions_for_flags_or_options "base-test" path1' -l 'path1' -rF
+complete -c 'base-test' -n '__base-test_should_offer_completions_for_flags_or_options "base-test" path2' -l 'path2' -rF
+complete -c 'base-test' -n '__base-test_should_offer_completions_for_flags_or_options "base-test" path3' -l 'path3' -rfka 'c1_fish c2_fish c3_fish'
+complete -c 'base-test' -n '__base-test_should_offer_completions_for_flags_or_options "base-test" one' -l 'one'
+complete -c 'base-test' -n '__base-test_should_offer_completions_for_flags_or_options "base-test" two' -l 'two'
+complete -c 'base-test' -n '__base-test_should_offer_completions_for_flags_or_options "base-test" custom-three' -l 'custom-three'
+complete -c 'base-test' -n '__base-test_should_offer_completions_for_flags_or_options "base-test"' -l 'kind-counter'
+complete -c 'base-test' -n '__base-test_should_offer_completions_for_flags_or_options "base-test"' -l 'rep1' -rfka ''
+complete -c 'base-test' -n '__base-test_should_offer_completions_for_flags_or_options "base-test"' -s 'r' -l 'rep2' -rfka ''
+complete -c 'base-test' -n '__base-test_should_offer_completions_for_positional "base-test" 1' -fka '(__base-test_custom_completion ---completion -- positional@0 (count (__base-test_tokens -pc)) (__base-test_tokens -tC))'
+complete -c 'base-test' -n '__base-test_should_offer_completions_for_positional "base-test" 2' -fka '(__base-test_custom_completion ---completion -- positional@1 (count (__base-test_tokens -pc)) (__base-test_tokens -tC))'
+complete -c 'base-test' -n '__base-test_should_offer_completions_for_flags_or_options "base-test" h help' -s 'h' -l 'help' -d 'Show help information.'
+complete -c 'base-test' -n '__base-test_should_offer_completions_for_positional "base-test" 3' -fa 'sub-command' -d ''
+complete -c 'base-test' -n '__base-test_should_offer_completions_for_positional "base-test" 3' -fa 'escaped-command' -d ''
+complete -c 'base-test' -n '__base-test_should_offer_completions_for_positional "base-test" 3' -fa 'help' -d 'Show subcommand help information.'
+complete -c 'base-test' -n '__base-test_should_offer_completions_for_flags_or_options "base-test sub-command" h help' -s 'h' -l 'help' -d 'Show help information.'
+complete -c 'base-test' -n '__base-test_should_offer_completions_for_flags_or_options "base-test escaped-command" o:n[e' -l 'o:n[e' -d 'Escaped chars: \'[]\\.' -rfka ''
+complete -c 'base-test' -n '__base-test_should_offer_completions_for_positional "base-test escaped-command" 1' -fka '(__base-test_custom_completion ---completion escaped-command -- positional@0 (count (__base-test_tokens -pc)) (__base-test_tokens -tC))'
+complete -c 'base-test' -n '__base-test_should_offer_completions_for_flags_or_options "base-test escaped-command" h help' -s 'h' -l 'help' -d 'Show help information.'
